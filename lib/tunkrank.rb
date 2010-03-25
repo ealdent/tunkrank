15049c7a5816c93077352ee31ff2d6ada34f2935
Jason Adams <jasonmadams@gmail.com>
2010-03-24 22:00:37 -0400

    bug fixes

--- lib/tunkrank.rb
+++ lib/tunkrank.rb
@@ -10,18 +10,16 @@ class TunkRank
   class << self
     def score(twitter_user)
       get("/score/#{twitter_user}.json")
+    rescue
+      raise TunkRankError, "Unable to fetch score for #{twitter_user}."
     end
 
     def raw_score(twitter_user)
-      score(twitter_user)['raw_tunkrank_score']
-    rescue
-      raise TunkRankError, "Score not calculated for #{twitter_user}."
+      score(twitter_user)['twitter_user']['raw_tunkrank_score']
     end
 
     def ranking(twitter_user)
-      score(twitter_user)['ranking']
-    rescue
-      raise TunkRankError, "Score not calculated for #{twitter_user}."
+      score(twitter_user)['twitter_user']['ranking']
     end
 
     def refresh(twitter_user)
